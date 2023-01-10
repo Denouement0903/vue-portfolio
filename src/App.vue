97f9eb770acdd1d59cc8756ed8e274bf0b02b38f
Simp4Hitagi <denorauten@gmail.com>
2023-01-10 09:36:26 +0200

    Created Vue'

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b968;
+}
+body {
+  border-radius: 4px;
+  background: linear-gradient(225deg, #67bad6, #7addff);
+  box-shadow:  -25px 25px 50px #2e535f,
+               25px -25px 50px #b6ffff;
+}
+</style>
