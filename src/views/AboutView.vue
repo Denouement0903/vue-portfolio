9702f887c6a09bc6be28e40c2c17032b56e87824
Simp4Hitagi <denorauten@gmail.com>
2023-03-19 22:51:23 +0200

    updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,42 +1,25 @@
 <template>
   <div>
-    <section class="about">
-      <h1 class="display-1 fw-bold animate__animated animate__fadeInDown">About Me</h1>
-      <div>
-        <div id="carouselExampleFade" class="carousel slide carousel-fade">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="https://i.postimg.cc/sDMvGK5P/D2.jpg" class="d-block rounded-5 w-25 m-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="https://i.postimg.cc/85rTPX2s/D1.jpg" class="d-block rounded-5 w-25 m-auto" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="https://i.postimg.cc/xdwWgfZT/D3.jpg" class="d-block rounded-5 w-25 m-auto" alt="...">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-
-      </div>
-      <div id="con_about" class="container-fluid py-5 bg-secondary bg-opacity-50 h-100 my-5">
-            <p class="lead shadow text-black my-5">I am an aspiring Web Developer with a keen interest in building projects to solve a need.
+    <!-- Section: Design Block -->
+    <section class="text-center">
+      <!-- Background image -->
+      <div class="p-5 bg-image"></div>
+      <!-- Background image -->
+      
+      <div class="card mx-4 mx-md-5 shadow-5-strong" style=" margin-top: -100px; background: hsla(0, 0%, 100%, 0.8);backdrop-filter: blur(30px); ">
+        <div class="info">
+          <h1 class="display-1">Who am I?</h1>
+		  <p class="lead shadow text-black my-5">I am an aspiring Web Developer with a keen interest in building projects to solve a need.
             What might that need be?
           </p>
-          <p class="lead shadow text-black">I specialize in Full Stack Development. This includes Front-End as well as Backend. </p>
+		  <p class="lead shadow text-black">I specialize in Full Stack Development. This includes Front-End as well as Backend. </p>
           <p class="lead shadow text-black">
             In my free time you'll find me in front of a computer screen learning something new. Be it a Language, Programming language, Mathematics, Philosophy, Mythology or even Classical Literature. My favorite pass time is learning something new! 
             Even so, I still enjoy sports and any outdoor activity just to be closer to nature.
           </p>
-          <!-- button to services/form -->
-          <button type="button" class="btn btn-outline-dark my-3"><router-link style="text-decoration:none" to="/contact">Let's Talk</router-link></button>
+
+		    <!-- button to services/form -->
+			<button type="button" class="btn btn-outline-dark my-3"><router-link style="text-decoration:none" to="/contact">Let's Talk</router-link></button>
             <!-- list group -->
             <h1 class="shadow">Personal Details</h1>
             <ul class="list-group border-2 border-dark list-unstyled gap-2">
@@ -46,13 +29,136 @@
               <li class="list-group-item-dark rounded-2 p-2">No criminal record</li>
               <li class="list-group-item-dark rounded-2 p-2">0303095358089</li>
             </ul>
-          </div>
-    </section>
+        </div>
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-8">
+
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section: Design Block -->
   </div>
 </template>
 <script>
 export default{
-
+  name: 'about'
 }
 
 </script>
+<style scoped>
+.bg-image{
+  background-image: url('https://i.postimg.cc/wjhN4RB1/11718.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: cover;
+  height: 300px;
+  box-shadow: 0px 8px 8px 0px rgb(248, 186, 62) inset;
+}
+
+.pt-100{
+    padding-top:100px;
+}
+.pb-100{
+    padding-bottom:100px;
+}
+.section-title {
+  margin-bottom: 60px;
+}
+.section-title p {
+	color: #777;
+	font-size: 16px;
+}
+.section-title h4 {
+	text-transform: capitalize;
+	font-size: 40px;
+	position: relative;
+	padding-bottom: 20px;
+	margin-bottom: 20px;
+	font-weight: 600;
+}
+.section-title h4:before {
+	position: absolute;
+	content: "";
+	width: 60px;
+	height: 2px;
+	background-color: orangered;
+	bottom: 0;
+	left: 50%;
+	margin-left: -30px;
+}
+.section-title h4:after {
+	position: absolute;
+	background-color: orangered;
+	content: "";
+	width: 10px;
+	height: 10px;
+	bottom: -4px;
+	left: 50%;
+	margin-left: -5px;
+	border-radius: 50%;
+}
+ul.timeline-list {
+	position: relative;
+	margin: 0;
+	padding: 0
+}
+ul.timeline-list:before {
+	position: absolute;
+	content: "";
+	width: 2px;
+	height: 100%;
+	background-color: orangered;
+	left: 50%;
+	top: 0;
+	-webkit-transform: translateX(-50%);
+	        transform: translateX(-50%);
+}
+ul.timeline-list li {
+	position: relative;
+	clear: both;
+	display: table;
+}
+.timeline_content {
+	border: 2px solid orangered;
+	background-color:#fff
+}
+ul.timeline-list li .timeline_content {
+	width: 45%;
+	color: #333;
+	padding: 30px;
+	float: left;
+	text-align: right;
+}
+ul.timeline-list li:nth-child(2n) .timeline_content {
+	float: right;
+	text-align: left;
+}
+.timeline_content h4 {
+	font-size: 22px;
+	font-weight: 600;
+	margin: 10px 0;
+}
+ul.timeline-list li:before {
+	position: absolute;
+	content: "";
+	width: 25px;
+	height: 25px;
+	background-color: orangered;
+	left: 50%;
+	top: 50%;
+	-webkit-transform: translate(-50%, -50%);
+	        transform: translate(-50%, -50%);
+	border-radius: 50%;
+}
+.timeline_content span {
+	font-size: 18px;
+	font-weight: 500;
+	font-family: poppins;
+	color: orangered;
+}
+
+
+</style>
+
+
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,88 +1,198 @@
 <template>
-    <div>
-        <h1 class="display-1 fw-bold animate__animated animate__fadeInDown">Resume</h1>
-        <section class="skills">
-            <h1 class="display-4 animate__animated animate__fadeInDown">Skills</h1>
-            <h2 class="text-primary">HTML</h2>
-            <div class="progress m-3" role="progressbar" aria-label="Default striped example" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 93%">93%</div>
-              </div>
-              <h2 class="text-success">Vue</h2>
-              <div class="progress m-3" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: 53%">53%</div>
-              </div>
-              <h2 class="text-info">Bootstrap</h2>
-              <div class="progress m-3" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 87%">87%</div>
-              </div>
-              <h2 class="text-warning">JavaScript</h2>
-              <div class="progress m-3" role="progressbar" aria-label="Warning striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 69%">69%</div>
-              </div>
-              <h2 class="text-danger">CSS</h2>
-              <div class="progress m-3" role="progressbar" aria-label="Danger striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 83%">83%</div>
-              </div>
-        </section>
-        <section class="work&education">          
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-
-                  <h3 class="mx-1 my-2 shadow">Education</h3>          
-                </button>
-              </h2>
-              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <h4 class="shadow">15 Jan 2017</h4>
-                  <h5>Start of highschool</h5>
-                  <p class="lead shadow text-black">I started grade 8 at Villiersdorp Secondary School.</p>
-                  <h4 class="shadow">Dec 2021</h4>
-                  <h5>End of highschool</h5>
-                  <p class="lead shadow text-black">I matriculated with a Bachelors pass.</p>
-                  <h6>Subjects</h6>
-                  <ul>
-                    <li class="list-unstyled">Afrikaans HL</li>
-                    <li class="list-unstyled">English FAL</li>
-                    <li class="list-unstyled">Maths</li>
-                    <li class="list-unstyled">Life Sciences</li>
-                    <li class="list-unstyled">Geography</li>
-                    <li class="list-unstyled">Computer Applications Technology</li>
-                    <li class="list-unstyled">Life Orientation</li>
-                  </ul>
-                  <h6>Achievements</h6>
-                  <p class="lead shadow text-black">I was Best in Afrikaans and English, 2nd best in Math, Geography and Computer Applications Technology, 3rd best in Life Science and achieved a distinction in English FAL in 2021 for Matric.</p>
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  <h3 class="mx-1 my-2 shadow">Experience</h3>
-                </button>
-              </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <h4 class="shadow">11 Feb 2020</h4>
-                  <h5>Start of Job at <strong>Tech Nasty</strong></h5>
-                  <p class="lead shadow text-black">During my time at Tech Nasty my designation was as an assistant in Information Technology and Computer repair.  This involved troubleshooting of hardware and software issues and repair as well as selling of technology and services. My key responsibilities included shop management and assistance.</p>
-                  <h4 class="shadow">15 Mar 2021</h4>
-                  <h6>End of Job</h6>
-                  <p class="lead shadow text-black">My reason for leaving was the indefinite closure of Tech Nasty.</p>
-                </div>
-              </div>
-            </div>
-          </div>         
-        </section>
-    </div>
+      <div class="container-fluid">
+        <div class="row bg-dark rounded-2">
+          <div class="col-md-6">
+               <h3 class="progress-title">HTML5 == 95%</h3>
+               <div class="progress orange">
+                   <div class="progress-bar" style="width:95%;">
+                       <div class="progress-value">95%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">CSS3 == 89%</h3>
+               <div class="progress blue">
+                   <div class="progress-bar" style="width:89%;">
+                       <div class="progress-value">89%</div>
+                   </div>
+               </div>
+        
+               <h3 class="progress-title">JS == 72%</h3>
+               <div class="progress yellow">
+                   <div class="progress-bar" style="width:72%;">
+                       <div class="progress-value">72%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">VUEJS == 79%</h3>
+               <div class="progress green">
+                   <div class="progress-bar" style="width:79%;">
+                       <div class="progress-value">79%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">MySQL == 91%</h3>
+               <div class="progress light-blue">
+                   <div class="progress-bar" style="width:91%;">
+                       <div class="progress-value">91%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">BOOTSTRAP == 93%</h3>
+               <div class="progress purple">
+                   <div class="progress-bar" style="width:93%;">
+                       <div class="progress-value">93%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">NODEJS == 69%</h3>
+               <div class="progress dark-green">
+                   <div class="progress-bar" style="width:69%;">
+                       <div class="progress-value">69%</div>
+                   </div>
+               </div>
+               <h3 class="progress-title">PYTHON == 57%</h3>
+               <div class="progress dark-yellow">
+                   <div class="progress-bar" style="width:57%;">
+                       <div class="progress-value">57%</div>
+                   </div>
+               </div>
+           </div>
+           <div class="col-md-6 my-5">
+            <h2 class="text-center display-1">SKILLS</h2>
+            <img class="w-75 rounded-5 mx-auto my-5 d-block" src="https://i.postimg.cc/x1whqSPr/jay-versace-typing.gif" alt="">
+            <h4 class="display-4 text-center">Fun Stuff</h4>
+           </div>
+        </div>
+        </div>
 </template>
 <script>
 export default {
-    name: 'resume'
-    
-}
+  name: 'resume'
+};
 </script>
 <style scoped>
-    
+.progress-title{
+color: #fff;
+text-transform: uppercase;
+letter-spacing: 2px;
+margin: 0 0 30px;
+}
+
+.progress{
+height: 10px;
+background: #000;
+border-radius: 0;
+box-shadow: none;
+margin-bottom: 40px;
+overflow: visible;
+position: relative;
+}
+
+.progress .progress-bar{
+background: #fff;
+border-radius: 0;
+position: relative;
+-webkit-animation: animate-positive 2s;
+animation: animate-positive 2s;
+}
+
+.progress .progress-bar:before{
+content: "";
+width: 100%;
+border-top: 10px double #f95738;
+border-right: 25px solid #000;
+position: absolute;
+top: 0;
+left: 0;
+}
+
+.progress .progress-value{
+font-size: 18px;
+font-weight: 700;
+color: #000;
+position: absolute;
+top: -32px;
+right: -5px;
+}
+
+.progress.dark-green .progress-bar:before{
+border-top-color: #68a063;
+}
+.progress.light-blue .progress-bar:before{
+border-top-color: #00758f;
+}
+.progress.purple .progress-bar:before{
+border-top-color: purple;
+}
+.progress.orange .progress-bar:before{
+border-top-color: orangered;
+}
+.progress.yellow .progress-bar:before{
+border-top-color: #ecb82d;
+}
+.progress.dark-yellow .progress-bar:before{
+border-top-color: #ffde57;
+}
+
+.progress.green .progress-bar:before{
+border-top-color: #42b883;
+}
+
+.progress.blue .progress-bar:before{
+border-top-color: #165692;
+}
+
+@-webkit-keyframes animate-positive{
+0% { width: 0; }
+}
+
+@keyframes animate-positive{
+0% { width: 0; }
+}
 </style>
+
+<section class="experience pt-100 pb-100" id="experience">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-8 mx-auto text-center">
+        <div class="section-title">
+          <h4>bootstrap 5 timeline</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+             <div class="col-xl-12">
+                <ul class="timeline-list">
+                   <!-- Single Experience -->
+                   <li>
+                      <div class="timeline_content">
+                         <span>2008</span>
+                         <h4>Intern Developer span</h4>
+                         <p>We gather your business and products information. We then determine the direction of the project and understand your goals and we combine your ideas with ours for an amazing website.</p>
+                      </div>
+                   </li>
+                   <!-- Single Experience -->
+                   <li>
+                      <div class="timeline_content">
+                      <span>2007-2012</span>
+                         <h4>Junior Developer</h4>
+                         <p>We gather your business and products information. We then determine the direction of the project and understand your goals and we combine your ideas with ours for an amazing website.</p>
+                      </div>
+                   </li>
+                   <!-- Single Experience -->
+                   <li>
+                      <div class="timeline_content">
+                      <span>2012-2015</span>
+                         <h4>Senior Developer</h4>
+                         <p>We gather your business and products information. We then determine the direction of the project and understand your goals and we combine your ideas with ours for an amazing website.</p>
+                      </div>
+                   </li>
+                   <!-- Single Experience -->
+                   <li>
+                      <div class="timeline_content">
+                      <span>2015-2018</span>
+                         <h4>Project Manager</h4>
+                         <p>We gather your business and products information. We then determine the direction of the project and understand your goals and we combine your ideas with ours for an amazing website.</p>
+                      </div>
+                   </li>
+                </ul>
+             </div>
+          </div>
+  </div>
+  </section>
