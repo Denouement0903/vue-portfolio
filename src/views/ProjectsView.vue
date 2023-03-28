09490e46aad2897f2dc7cd18a9727f3a04674db4
Simp4Hitagi <denorauten@gmail.com>
2023-03-28 16:48:29 +0200

    Added More Projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,9 +4,9 @@
         <div class="container">
             <div class="row">
                     <div class="col" v-for="project in projectsArray" :key="project">
-                        <div class="card mx-auto my-3" style="width: 18rem;">
-                                <img :src=project.image class="card-img-top" alt="...">
-                                <div class="card-body">
+                        <div class="card mx-auto my-3 border border-dark border-4 " style="width: 18rem;">
+                                <img :src=project.image class="card-img-top" alt="..." style="height: 13rem;">
+                                <div class="card-body border border-warning border-4">
                                   <h5 class="card-title">{{project.name}}</h5>
                                   <p class="card-text">{{project.description}}</p>
                                   <a :href=project.netlify class="btn btn-primary m-3">Netlify</a>
@@ -27,6 +27,22 @@ export default {
             projectsArray: [
                 {
                     id: 1,
+                    name: "Manga Site",
+                    description: "A E-commerce project using VUEJS and NODE JS and API Functionality",
+                    image: "https://i.postimg.cc/yxs7RY5z/Screenshot-2023-03-28-161820.png",
+                    netlify: "https://mangako-deno-rautenbach.netlify.app/",
+                    github: "https://github.com/Simp4Hitagi/Capstone-Mangako"
+                },
+                {
+                    id: 2,
+                    name: "Figures Site",
+                    description: "A collaborative E-commerce project using VUEJS and NODE JS",
+                    image: "https://i.postimg.cc/DwMsPtnN/Screenshot-2023-03-28-162401.png",
+                    netlify: "",
+                    github: "https://github.com/Simp4Hitagi/Otaku"
+                },
+                {
+                    id: 3,
                     name: "Calculator",
                     description: "A basic calculator app built using HTML, CSS, Bootstrap and Javascript",
                     image: "https://i.postimg.cc/fbN8V6q4/calculator.png",
@@ -34,15 +50,15 @@ export default {
                     github: "https://github.com/Simp4Hitagi/My_Calculator"
                 },
                 {
-                    id: 2,
-                    name: "Portfolio",
-                    description: "Portfolio project using HTML, CSS and Bootstrap",
-                    image: "https://i.postimg.cc/cJ2qxRPr/portfolio.png",
-                    netlify: "https://deno-portfolio1.netlify.app",
-                    github: "https://github.com/Simp4Hitagi/Final_Portfolio_Project"
+                    id: 4,
+                    name: "Vue Portfolio",
+                    description: "A updated of the current portfolio project using Vue, JS, HTML and CSS ",
+                    image: "https://i.postimg.cc/gcTMrB6g/Screenshot-2023-03-28-162020.png",
+                    netlify: "https://deno-vue-portfolio.netlify.app/",
+                    github: "https://github.com/Simp4Hitagi/vue-portfolio"
                 },
                 {
-                    id: 3,
+                    id: 5,
                     name: "Point of Sale System",
                     description: "E-Commerce ste with a CRUD system primarily built using JS, HTML and CSS",
                     image: "https://i.postimg.cc/cH96MZhK/pos-system.png",
@@ -50,7 +66,7 @@ export default {
                     github: "https://github.com/Simp4Hitagi/PointOfSale_System"
                 },
                 {
-                    id: 4,
+                    id: 6,
                     name: "Tribute Page",
                     description: "A basic page to showcase cool art using mainly HTML and CSS",
                     image: "https://i.postimg.cc/3Jk3TwkS/tribute-page.png",
@@ -58,21 +74,25 @@ export default {
                     github: "https://github.com/Simp4Hitagi/excercise"
                 },
                 {
-                    id: 5,
-                    name: "Vue Portfolio",
-                    description: "A portfolio project using Vue, JS, HTML and CSS",
-                    image: "https://i.postimg.cc/XYLVSZGX/vue-portfolio.png",
-                    netlify: "deno-vue-portfolio.netlify.app",
-                    github: "https://github.com/Simp4Hitagi/vue-portfolio"
+                    id: 7,
+                    name: "Portfolio",
+                    description: "My first Portfolio project using HTML, CSS and Bootstrap",
+                    image: "https://i.postimg.cc/cJ2qxRPr/portfolio.png",
+                    netlify: "https://deno-portfolio1.netlify.app",
+                    github: "https://github.com/Simp4Hitagi/Final_Portfolio_Project"
                 },
+
+
+                
                 {
-                    id: 6,
+                    id: 8,
                     name: "Animation Site",
                     description: "A animation project using HTML and CSS",
                     image: "https://i.postimg.cc/YqgRP8Bp/Screenshot-2023-01-14-133534.png",
                     netlify: "animation-deno.netlify.app",
                     github: "https://github.com/Simp4Hitagi/animation"
-                }
+                },
+
             ]
         }
     }
