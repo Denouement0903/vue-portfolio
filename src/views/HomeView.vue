b595ced9702766626469cebf2e69cbf36902790a
Simp4Hitagi <denorauten@gmail.com>
2023-08-21 03:37:17 +0200

    Added glitch on home text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,11 @@
 		<section id="home" class="home my-5 py-5">
 		  <div class="container-fluid-mouse" style="height: 800px">
 			<div class="row d-flex align-items-center text-center field">
-				<h1 class="display-1 intro my-5">Welcome</h1>
+				<div id="welcome" class="my-5">
+					<h1 class="welcome display-1 text-light my-3">Hello</h1>
+					<h1 class="welcome display-1 text-light my-3">Welcome</h1>
+					<h1 class="welcome display-1 text-light my-3">Hi</h1>
+				  </div>
 				  <div class="mouse"></div>
 			</div>
 		  </div>
@@ -539,6 +543,200 @@ links.forEach(link => {
 
 <style scoped>
 
+#welcome {
+	width: 70vw;
+	margin: 30vw auto;
+  }
+  
+  .welcome {
+	text-align: center;
+	color: rgb(255, 255, 255);
+	text-shadow: 2px 2px 0px #000000;
+
+	margin: 0;
+	line-height: 0;
+	/*Animation*/
+	
+	animation: glitch1 2.5s infinite;
+  }
+  
+  .welcome:nth-child(2) {
+	color: white;
+	animation: glitch2 2.5s infinite;
+	text-shadow: 2px 2px 0px #000000;
+  }
+  
+  .welcome:nth-child(3) {
+	color: rgb(255, 255, 255);
+	animation: glitch3 2.5s infinite;
+	text-shadow: 2px 2px 0px #000000;
+  }
+  /*Keyframes*/
+  
+  @keyframes glitch1 {
+	0% {
+	  transform: none;
+	  opacity: 1;
+	}
+	7% {
+	  transform: skew(-0.5deg, -0.9deg);
+	  opacity: 0.75;
+	}
+	10% {
+	  transform: none;
+	  opacity: 1;
+	}
+	27% {
+	  transform: none;
+	  opacity: 1;
+	}
+	30% {
+	  transform: skew(0.8deg, -0.1deg);
+	  opacity: 0.75;
+	}
+	35% {
+	  transform: none;
+	  opacity: 1;
+	}
+	52% {
+	  transform: none;
+	  opacity: 1;
+	}
+	55% {
+	  transform: skew(-1deg, 0.2deg);
+	  opacity: 0.75;
+	}
+	50% {
+	  transform: none;
+	  opacity: 1;
+	}
+	72% {
+	  transform: none;
+	  opacity: 1;
+	}
+	75% {
+	  transform: skew(0.4deg, 1deg);
+	  opacity: 0.75;
+	}
+	80% {
+	  transform: none;
+	  opacity: 1;
+	}
+	100% {
+	  transform: none;
+	  opacity: 1;
+	}
+  }
+  
+  @keyframes glitch2 {
+	0% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	7% {
+	  transform: translate(-2px, -3px);
+	  opacity: 0.5;
+	}
+	10% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	27% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	30% {
+	  transform: translate(-5px, -2px);
+	  opacity: 0.5;
+	}
+	35% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	52% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	55% {
+	  transform: translate(-5px, -1px);
+	  opacity: 0.5;
+	}
+	50% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	72% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	75% {
+	  transform: translate(-2px, -6px);
+	  opacity: 0.5;
+	}
+	80% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	100% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+  }
+  
+  @keyframes glitch3 {
+	0% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	7% {
+	  transform: translate(2px, 3px);
+	  opacity: 0.5;
+	}
+	10% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	27% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	30% {
+	  transform: translate(5px, 2px);
+	  opacity: 0.5;
+	}
+	35% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	52% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	55% {
+	  transform: translate(5px, 1px);
+	  opacity: 0.5;
+	}
+	50% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	72% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	75% {
+	  transform: translate(2px, 6px);
+	  opacity: 0.5;
+	}
+	80% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+	100% {
+	  transform: none;
+	  opacity: 0.25;
+	}
+  }
 .navbar-nav a::before, .navbar-nav a::after {
 	display: inline-block;
 	opacity: 0;
